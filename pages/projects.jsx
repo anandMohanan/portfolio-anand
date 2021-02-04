46bb77bf59259f30f25c2bda7fb14ea108f874fb
bruh711 <ananthkvmohanan@gmail.com>
2021-02-04 09:05:28 +0530

    updated projects

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,13 +1,13 @@
-import styles from '../styles/projects.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
+import styles from "../styles/projects.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Project = () => {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Projects</h1>
-      <div className={styles.link} style={{ marginTop: '20px' }}>
-        <Link href='/'> &#8592; Home</Link>
+      <div className={styles.link} style={{ marginTop: "20px" }}>
+        <Link href="/"> &#8592; Home</Link>
       </div>
 
       <div className={styles.grid}>
@@ -16,14 +16,16 @@ const Project = () => {
           <p>A Clothing E-commerce web application</p>
           <p>Stack Used: React--Firebase--Styled Components</p>
           <a
-            href='https://github.com/ananth7112001/clothing-react'
-            style={{ marginRight: '10px' }}>
-            <Image height='30' width='30' src='/file.svg' alt='github' />
+            href="https://github.com/anandMohanan/clothing-react"
+            style={{ marginRight: "10px" }}
+          >
+            <Image height="30" width="30" src="/file.svg" alt="github" />
           </a>
           <a
-            href='https://clothing-react-by-anand.herokuapp.com/'
-            style={{ marginRight: '10px' }}>
-            <Image height='30' width='30' src='/web.svg' alt='live' />
+            href="https://clothing-react-by-anand.herokuapp.com/"
+            style={{ marginRight: "10px" }}
+          >
+            <Image height="30" width="30" src="/web.svg" alt="live" />
           </a>
         </div>
 
@@ -35,28 +37,32 @@ const Project = () => {
           </p>
           <p>Stack Used: React--Mongo Db--Mongoose--Express</p>
           <a
-            href='https://github.com/ananth7112001/post-react-express-mongodb'
-            style={{ marginRight: '10px' }}>
-            <Image height='30' width='30' src='/file.svg' alt='github' />
+            href="https://github.com/anandMohanan/post-react-express-mongodb"
+            style={{ marginRight: "10px" }}
+          >
+            <Image height="30" width="30" src="/file.svg" alt="github" />
           </a>
           <a
-            href='https://post-react-anandkv.netlify.app/'
-            style={{ marginRight: '10px' }}>
-            <Image height='30' width='30' src='/web.svg' alt='live' />
+            href="https://post-react-anandkv.netlify.app/"
+            style={{ marginRight: "10px" }}
+          >
+            <Image height="30" width="30" src="/web.svg" alt="live" />
           </a>
         </div>
 
         <div className={styles.card}>
-          <h3>Music</h3>
+          <h3>Chilli bot</h3>
           <p>
-            A discord music bot which allows users to play music from youtube
-            and soundcloud.It also has moderation commands
+            Chilli is a multi functionality discord bot with features like
+            music(with filters),moderation,economy,roleplay fun commands.
+            Currently running in 10 servers with over 2000 users.
           </p>
           <p>Stack Used: Discord.js</p>
           <a
-            href='https://github.com/ananth7112001/music-bot'
-            style={{ marginRight: '10px' }}>
-            <Image height='30' width='30' src='/file.svg' alt='github' />
+            href="https://github.com/anandMohanan/chilli-bot"
+            style={{ marginRight: "10px" }}
+          >
+            <Image height="30" width="30" src="/file.svg" alt="github" />
           </a>
         </div>
 
@@ -68,9 +74,10 @@ const Project = () => {
           </p>
           <p>Stack Used: Lightning Web Components--Js--css</p>
           <a
-            href='https://github.com/ananth7112001/lwc-conference-app'
-            style={{ marginRight: '10px' }}>
-            <Image height='30' width='30' src='/file.svg' alt='github' />
+            href="https://github.com/anandMohanan/lwc-conference-app"
+            style={{ marginRight: "10px" }}
+          >
+            <Image height="30" width="30" src="/file.svg" alt="github" />
           </a>
         </div>
       </div>
